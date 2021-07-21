61e3049c10d339c2be460c02a37b122341fd0dfb
DevDaveFrame <dframe22@gmail.com>
2021-07-21 15:29:34 -0700

    added ad to NewsPost and created AdvertisementAdmin and AdvertisementForm

--- apps/advertising/admin.py
+++ apps/advertising/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
+from django.contrib import admin, forms
+from advertising.models import Advertisement
+from news.models import NewsPost
 
 # Register your models here.
+
+
+class AdvertisementForm(forms.ModelForm):
+    newspost = forms.ModelMultipleChoiceField(queryset=NewsPost.objects.all(), widget=forms.CheckboxSelectMultiple)
+    model = Advertisement
+    fields = '__all__'
+
+    def save(self, commit=True):
+        instance = super().save(commit)
+        instance.save()
+        for post in self.cleaned_data['newspost']:
+            instance.newspost_set.add(post)
+        return instance
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    form = AdvertisementForm
+    list_display = ['company_name', 'copy']
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
